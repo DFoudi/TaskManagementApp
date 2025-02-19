6238b91be0fc0e70fa37f1c6daceb25e3e14e04a
DFoudi <david.foudi1@gmail.com>
2025-02-19 02:05:34 +0100

    Remove comments

--- TaskManagementApp/Services/TaskService.cs
+++ TaskManagementApp/Services/TaskService.cs
@@ -6,19 +6,6 @@ namespace TaskManagementApp.Services
 {
     public class TaskService
     {
-        // POURQUOI UN SERVICE POURQUOI PAS UN CONTROLLER DIRECTEMENT ? COMMENT UN ALGORITHME PEUT FONCTIONNER EN DEHORS DUN CONTROLLER ? CEST VRAI NORMALEMENT LES MECANISMES
-        // FONCTIONNENT DANS LES CONTROLLERS CEST LA BAS QUON A NOS ALGO NOS FONCTIONS ALORS POURQUOI UN DOSSIER SERVICE ET DANS CE DOSSIER UNE CLASS QUI PEUT LUI FAIRE TOURNER
-        // DU CODE ET CA SANS CONTROLLER ??
-
-
-        // Explication :
-        // TaskService est responsable de la logique métier des tâches.
-        // Il utilise un ITaskRepository pour interagir avec la base de données(qu'on va mocker dans les tests).
-
-        // Ok donc ITaskRepository est une interface propre à .net c'est un nom qui est donné par .Net ou par nous ? Réponse courte
-
-        // EXPLIQUE MOI LE CODE SINON JE NE SERAI PAS CAPABLE DE LE REFAIRE TOUT SEUL SANS TON AIDE EN ENTREPRISE
-
         private readonly ITaskRepository _taskRepository;
 
         public TaskService(ITaskRepository taskRepository)
